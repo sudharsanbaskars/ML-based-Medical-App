67564aa17d04fab88103980180f0af08c5e69ca2
sudharsanbaskars <71257512+sudharsanbaskars@users.noreply.github.com>
2020-10-13 17:59:11 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import source.app_heart
+import source.app_liver
+import source.app_diabetes
+import source.app_home
+import source.app_about
+import awesome_streamlit as ast
+
+
+ast.core.services.other.set_logging_format()
+
+pages = {
+         "Home":source.app_home,
+         "About":source.app_about,
+          "Diabetes":source.app_diabetes,
+         "Heart":source.app_heart,
+         "Liver":source.app_liver}
+
+def main():
+    st.sidebar.header("Navigation")
+
+
+    selection = st.sidebar.radio("Go to", list(pages.keys()))
+    page = pages[selection]
+    page.main()
+    #with st.spinner(f"Loading {selection} ..."):
+        #ast.shared.components.write_page(page)
+    st.sidebar.title("Creators")
+    st.sidebar.write("""
+    - **B.Sudharsan**
+    - **K.Gowri Shankar**
+    - **S.Sriaakash**
+    - **M.Hariharan**
+            """)
+
+
+if __name__ == "__main__":
+    main()
